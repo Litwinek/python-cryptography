4c80426300496b627e5cec1bdccfe9e91cbfd109
Kacper <freeny@wp.pl>
2023-12-11 14:39:14 +0100

    Caesar cipher script

---
+++ caesar-cipher/caesar_cipher.py
@@ -0,0 +1,28 @@
+ALPHABET = ' !ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+KEY = 3
+
+
+def caesar_encrypt(plain_text):
+    cipher_text = ''
+    plain_text = plain_text.upper()
+    for c in plain_text:
+        index = ALPHABET.find(c)
+        index = (index + KEY) % len(ALPHABET)
+        cipher_text = cipher_text + ALPHABET[index]
+    return cipher_text
+
+
+def caesar_decrypt(cipher_text):
+    plain_text = ''
+    for c in cipher_text:
+        index = ALPHABET.find(c)
+        index = (index - KEY) % len(ALPHABET)
+        plain_text = plain_text + ALPHABET[index]
+    return plain_text
+
+
+if __name__ == '__main__':
+    m = 'My cipher'
+    encrypted = caesar_encrypt(m)
+    print(encrypted)
+    print(caesar_decrypt(encrypted))
